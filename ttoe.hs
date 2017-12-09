ccd5bb7658ec860801f0c27e0f76a843dc10dbda
Art Deineka <art.deineka@darkruby.co.uk>
2017-12-09 15:25:45 +0000

    io for ttoe

--- ttoe.hs
+++ ttoe.hs
@@ -1,23 +1,25 @@
 import Data.List
 import Data.List.Split
+import Data.Char
+import System.IO
 
 data Player = X | O
-showPlayer :: Player -> String
-showPlayer X = "☓"
-showPlayer O = "○"
+show_player :: Player -> String
+show_player X = "☓"
+show_player O = "○"
 instance Show Player where
-  show p = showPlayer p
+  show p = show_player p
 instance Eq Player where
   (X) == (X) = True
   (O) == (O) = True
   _   == _   = False
 
 data Cell = Occupied Player | Empty
-showCell :: Cell -> String
-showCell (Occupied p) = "[" ++ show p ++ "]"
-showCell Empty = "_"
+show_cell :: Cell -> String
+show_cell (Occupied p) = "[" ++ show p ++ "]"
+show_cell Empty = "[_]"
 instance Show Cell where
-  show c = showCell c
+  show c = show_cell c
 instance Eq Cell where
   (Occupied p1) == (Occupied p2) = p1 == p2
   Empty         == Empty         = True
@@ -26,6 +28,10 @@ instance Eq Cell where
 type Field = [Cell]
 type Eval = (Int, Maybe Int)
 type State = (Maybe Player, [Int])
+
+show_field :: Field -> String
+show_field field = intercalate "\n" (map unwords (chunksOf dim (map show field))) ++ "\n" where
+  dim = dimension field
 
 new_field :: Int -> Field
 new_field side | side > 0 = [Empty | _ <- [1..side^2]]
@@ -49,13 +55,16 @@ winner player field = any (occupied_line player) (winning_combinations field)
 take_every_nth :: Int -> [a] -> [a]
 take_every_nth n = map snd . filter ((== n) . fst) . zip (cycle [1..n])
 
+dimension :: Field -> Int
+dimension field= (round.sqrt.fromIntegral.length) field
+
 winning_combinations :: Field -> [[Cell]]
 winning_combinations field = horizontals ++ verticals ++ diagonal1 ++ diagonal2 where
-                             dimension = (round.sqrt.fromIntegral.length) field
-                             horizontals = chunksOf dimension field
+                             dim       = dimension field
+                             horizontals = chunksOf dim field
                              verticals = transpose horizontals
-                             diagonal1 = [(head field) : take_every_nth (dimension + 1) (tail field)]
-                             diagonal2 = [take dimension (take_every_nth (dimension - 1) (tail field))]
+                             diagonal1 = [(head field) : take_every_nth (dim + 1) (tail field)]
+                             diagonal2 = [take dim (take_every_nth (dim - 1) (tail field))]
 
 has_winner :: Field -> (Maybe Player)
 has_winner field | winner X field = Just X
@@ -68,6 +77,13 @@ possible_moves field = [i | (c,i) <- zip field [0..], c == Empty]
 board_state :: Field -> State
 board_state field = (has_winner field, possible_moves field)
 
+game_over :: Field -> Bool
+game_over field = case board_state field of
+    (Just X, _) -> True
+    (Just O, _) -> True
+    (_, [])     -> True
+    otherwise   -> False
+
 opposite :: Player -> Player
 opposite X = O
 opposite O = X
@@ -77,7 +93,51 @@ minimax field player depth = case board_state field of
   (Nothing, [])    -> (depth - 10, Nothing)
   (Just O, _)      -> (depth - 10, Nothing)
   (Just X, _)      -> (10 - depth, Nothing)
-  (Nothing, moves) -> pick $ sort [(fst $ minimax (make_next_move n) opposite_player (depth + 1), Just n) | n <- moves] where
+  (Nothing, moves) -> pick $ sort [(fst $ minimax (make_next_move n) opposite_player (depth + 1), Just n)
+                                  | n <- moves] where
     pick = if player == O then head else last
     opposite_player = opposite player
     make_next_move = make_move player field
+
+print_field :: Field -> IO ()
+print_field field = do
+  putStr "\n"
+  putStr (show_field field)
+  putStr "\n"
+
+enter_move :: Field -> IO Int
+enter_move field = do
+  putStr "Enter move [0..8]: "
+  move_str <- getLine
+  let move = read move_str :: Int
+  if (elem move (possible_moves field)) then
+    return move
+  else
+    do enter_move field
+
+cpu :: Field -> IO Field
+cpu field = do
+  let evl = minimax field O 0
+  case snd evl of
+    Just move -> return (make_move O field move)
+    _         -> return field
+
+play :: Field -> Player -> IO ()
+play field player = do
+  print_field field
+  if game_over field then
+    do putStr "Game over"
+  else
+    case player of
+      X -> do
+        move <- enter_move field;
+        let field' = make_move X field move;
+        play field' O;
+      O -> do
+        field' <- cpu field;
+        play field' X;
+
+main = do
+  hSetBuffering stdout NoBuffering
+  hSetBuffering stdin NoBuffering
+  play (new_field 3) X
